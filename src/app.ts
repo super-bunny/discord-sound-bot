7c6a9b428f962e72f612b868e61bba945df3137f
Clovis de Villars <clovis.devillars@gmail.com>
2020-10-14 19:32:38 +0200

    Add "/p" command

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,48 @@
 require('dotenv').config()
 import * as env from 'env-var'
+import * as Discord from 'discord.js'
+import MediaManager from './classes/MediaManager'
 
 env.get('MEDIA_FOLDER').required().asString()
 env.get('DISCORD_TOKEN').required().asString()
+env.get('DISCORD_ROLE_NAME').required().asString()
+
+async function main() {
+  const client = new Discord.Client()
+  const mediaManager = await MediaManager.init(process.env.MEDIA_FOLDER)
+
+  client.on('ready', () => {
+    console.log('I am ready!')
+  })
+
+  client.on('message', async message => {
+    // Voice only works in guilds, if the message does not come from a guild,
+    // we ignore it
+    if (!message.guild) return
+
+    if (message.content.split(' ').shift() === '/p') {
+      // Prevent non authorized member to execute command
+      if (!message.member.roles.cache.some(role => role.name === process.env.DISCORD_ROLE_NAME)) {
+        return
+      }
+      // Only try to join the sender's voice channel if they are in one themselves
+      if (message.member.voice.channel) {
+        const media = mediaManager.getByFileName(message.content.split(' ').pop())
+        if (media) {
+          const connection = await message.member.voice.channel.join()
+          const dispatcher = connection.play(media)
+        } else {
+          message.reply('Media not found :(')
+        }
+      } else {
+        message.reply('You need to join a voice channel first!')
+      }
+    }
+  })
+
+  await client.login(process.env.DISCORD_TOKEN)
+}
+
+main()
+  .then()
+  .catch(error => console.error(error))
