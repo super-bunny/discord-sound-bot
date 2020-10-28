78ff9efee427229177845aac65a6710f6a6ba40a
Clovis de Villars <clovis.devillars@gmail.com>
2020-10-28 22:40:13 +0100

    Use Bot class to manage app state
    Load bot JSON file config and init Bot class with it

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,19 @@
 require('dotenv').config()
 import chokidar from 'chokidar'
+import Bot from './classes/Bot'
 import * as env from 'env-var'
-import * as Discord from 'discord.js'
-import MediaManager from './classes/MediaManager'
 import { getBotOwner } from './utils'
 
+env.get('CONFIG_FILE').asUrlString
 env.get('MEDIA_FOLDER').required().asString()
 env.get('DISCORD_TOKEN').required().asString()
 env.get('DISCORD_OWNER_ID').required().asString()
 env.get('DISCORD_ROLE_NAME').required().asString()
 
+const config = require(process.env.CONFIG_FILE || '../config.json')
+
 async function main() {
-  const client = new Discord.Client()
-  const mediaManager = await MediaManager.init(process.env.MEDIA_FOLDER)
+  const bot = await Bot.init(config)
 
   const watcher = chokidar.watch(process.env.MEDIA_FOLDER, {
     ignored: /^\./,
@@ -22,27 +23,27 @@ async function main() {
   })
     .on('add', (path) => {
       console.log(`File ${ path.split('/').pop() } added, refreshing media list...`)
-      mediaManager.refresh()
+      bot.mediaManager.refresh()
         .then(() => console.log('Media list refreshed'))
-        .then(() => getBotOwner(client))
+        .then(() => getBotOwner(bot.discord))
         .then(owner => owner.send(`File **${ path.split('/').pop() }** added. :new:\nMedia list refreshed :recycle:`))
     })
     .on('unlink', (path) => {
       console.log(`File ${ path.split('/').pop() } removed, refreshing media list...`)
-      mediaManager.refresh()
+      bot.mediaManager.refresh()
         .then(() => console.log('Media list refreshed'))
-        .then(() => getBotOwner(client))
+        .then(() => getBotOwner(bot.discord))
         .then(owner => owner.send(`File **${ path.split('/').pop() }** removed. :wastebasket:\nMedia list refreshed :recycle:`))
     })
     .on('error', (error) => {
       console.error('Chokidar error happened', error)
     })
 
-  client.on('ready', () => {
+  bot.discord.on('ready', () => {
     console.log('Discord bot ready!')
   })
 
-  client.on('message', async message => {
+  bot.discord.on('message', async message => {
     // Voice only works in guilds, if the message does not come from a guild,
     // we ignore it
     if (!message.guild) return
@@ -55,7 +56,7 @@ async function main() {
       // Only try to join the sender's voice channel if they are in one themselves
       if (message.member.voice.channel) {
         const queryTokens = message.content.split(' ')
-        const [media] = mediaManager.getBySearch(queryTokens.slice(1, queryTokens.length).join(' '))
+        const [media] = bot.mediaManager.getBySearch(queryTokens.slice(1, queryTokens.length).join(' '))
         if (media) {
           const connection = await message.member.voice.channel.join()
           const dispatcher = connection.play(media)
@@ -66,24 +67,24 @@ async function main() {
         message.reply('You need to join a voice channel first!')
       }
     } else if (message.content.split(' ').shift() === '/list') {
-      message.reply('```' + mediaManager.getFilenameList().join('\n') + '```')
+      message.reply('```' + bot.mediaManager.getFilenameList().join('\n') + '```')
     } else if (message.content.split(' ').shift() === '/r') {
       if (message.member.voice.channel) {
-        const randomIndex = Math.trunc(Math.random() * mediaManager.data.length)
-        const media = mediaManager.data[randomIndex]
+        const randomIndex = Math.trunc(Math.random() * bot.mediaManager.data.length)
+        const media = bot.mediaManager.data[randomIndex]
         const connection = await message.member.voice.channel.join()
         const dispatcher = connection.play(media)
-        message.reply(`Playing *${ mediaManager.getFilenameList()[randomIndex] }*`)
+        message.reply(`Playing *${ bot.mediaManager.getFilenameList()[randomIndex] }*`)
       } else {
         message.reply('You need to join a voice channel first!')
       }
     }
   })
 
-  client.on('voiceStateUpdate', (oldMember, newMember) => {
+  bot.discord.on('voiceStateUpdate', (oldMember, newMember) => {
     // If a member disconnect from voice channel
     if (oldMember.channelID !== null && oldMember.channelID !== newMember.channelID) {
-      const connection = client.voice.connections.find(connection => connection.channel.id === oldMember.channelID)
+      const connection = bot.discord.voice.connections.find(connection => connection.channel.id === oldMember.channelID)
       // If last channel member is this bot
       if (connection && oldMember.channel.members.array().length === 1) {
         connection.disconnect()
@@ -91,7 +92,7 @@ async function main() {
     }
   })
 
-  await client.login(process.env.DISCORD_TOKEN)
+  await bot.discord.login(process.env.DISCORD_TOKEN)
 }
 
 main()
